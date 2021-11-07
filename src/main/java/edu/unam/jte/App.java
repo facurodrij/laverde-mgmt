e13be8297233fd0bf0b3c915b10c55f60637cc84
FacuRodriJ <facurodri00@gmail.com>
2021-11-07 19:58:25 -0300

    Update App.java

--- src/main/java/edu/unam/jte/App.java
+++ src/main/java/edu/unam/jte/App.java
@@ -3,8 +3,12 @@ package edu.unam.jte;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.staticfiles.Location;
-//import jakarta.persistence.Persistence;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import java.util.Collections;
+import edu.unam.jte.controladores.*;
+import edu.unam.jte.paginas.*;
+import edu.unam.jte.repositorios.*;
 
 // para usar routes
 // import static io.javalin.apibuilder.ApiBuilder.*;
@@ -21,7 +25,11 @@ public class App {
      */
     public static void main(String[] args) {
 
-        //var emf = Persistence.createEntityManagerFactory("LaVerdeSA");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("LaVerdeSA");
+
+        // repositorios y controladores
+        var productoresRepositorio = new JPAProductoresRepositorio(emf);
+        var productoresControlador = new ProductoresControlador(productoresRepositorio);
         
         // creo servidor
         Javalin app = Javalin.create(config -> {
@@ -32,7 +40,13 @@ public class App {
 
 
         // defino rutas
-        app.get("/", App::mostrarIndex); // muestra el index       
+        app.get("/", App::mostrarIndex); // muestra el index
+        app.post("/", App::validarUsuario); // "valida usuario"
+        app.get("/productores", productoresControlador::listar); // lista los productores
+        app.get("/productores/nuevo", productoresControlador::nuevo); // pantalla de nuevo curso
+        app.post("/productores", productoresControlador::crear); // crea un curso
+        app.get("/productores/{id}", productoresControlador::modificar); // devuelve datos del curso
+        app.delete("/productores/{id}", productoresControlador::borrar); // borra un curso 
 
     }
 
@@ -41,6 +55,27 @@ public class App {
      * @param ctx Contexto de la petición
      */
     private static void mostrarIndex(Context ctx) {
-        ctx.render("index.jte", Collections.singletonMap("modelo", null));
-    } 
+        var modelo = new ModeloIndex();
+        // controlo por cookie
+        if (ctx.cookie("nombreUsuario") != null) {
+            modelo.nombreUsuario = ctx.cookie("nombreUsuario");
+        } else {
+            modelo.nombreUsuario = "";
+        }
+        ctx.render("index.jte", Collections.singletonMap("modelo", modelo));
+    }
+
+    /** 
+     * @param ctx Contexto de la petición
+     */
+    private static void validarUsuario(Context ctx) {
+        // obtengo valor enviado en el formulario
+        var valor = ctx.formParamAsClass("nombreUsuario", String.class).get();
+        // creo el cookie (antes se debe validar sobre la base de datos o alguna otra forma)
+        // recibo el elemento y hago un trim para guardarlo en el cookie
+        ctx.cookie("nombreUsuario", valor.trim());
+        // redirecciono a / 
+        ctx.redirect("/");
+    }    
 }
+
