94c5525a6f9674f207c3f278b6dbf34b4c1f6038
FacuRodriJ <facurodri00@gmail.com>
2021-11-14 01:24:55 -0300

    Ajuste diseño modelo Productor.

---
+++ jte-classes/gg/jte/generated/ondemand/productor/JtelistarGenerated.java
@@ -0,0 +1,49 @@
+package gg.jte.generated.ondemand.productor;
+import edu.unam.jte.paginas.ModeloProductores;
+public final class JtelistarGenerated {
+	public static final String JTE_NAME = "productor/listar.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,3,3,3,5,5,7,9,23,26,31,63,63,65,65,65,66,66,66,67,67,67,68,68,68,70,70,70,73,73,73,78,78,83,85,90,92,94,94};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, ModeloProductores modelo) {
+		jteOutput.writeContent("\r\n");
+		gg.jte.generated.ondemand.tag.JteheadGenerated.render(jteOutput, jteHtmlInterceptor);
+		jteOutput.writeContent("\r\n\r\n  ");
+		gg.jte.generated.ondemand.tag.JtesidebarGenerated.render(jteOutput, jteHtmlInterceptor);
+		jteOutput.writeContent("\r\n\r\n  ");
+		jteOutput.writeContent("\r\n  <div class=\"content-wrapper\">\r\n    ");
+		jteOutput.writeContent("\r\n    <section class=\"content-header\">\r\n      <div class=\"container-fluid\">\r\n        <div class=\"row mb-2\">\r\n          <div class=\"col-sm-6\">\r\n            <h1>Productores</h1>\r\n          </div>\r\n          <div class=\"col-sm-6\">\r\n            <ol class=\"breadcrumb float-sm-right\">\r\n              <li class=\"breadcrumb-item\"><a href=\"/\">Inicio</a></li>\r\n              <li class=\"breadcrumb-item active\">Productores</li>\r\n            </ol>\r\n          </div>\r\n        </div>\r\n      </div>");
+		jteOutput.writeContent("\r\n    </section>\r\n\r\n    ");
+		jteOutput.writeContent("\r\n    <section class=\"content\">\r\n      <div class=\"container-fluid\">\r\n        <div class=\"row\">\r\n          <div class=\"col-12\">\r\n            ");
+		jteOutput.writeContent("\r\n            <div class=\"card\">\r\n              <div class=\"card-header\">\r\n                <h3 class=\"card-title\">Lista de Productores</h3>\r\n\r\n                <div class=\"card-tools\">\r\n                  <button type=\"button\" class=\"btn btn-tool\" data-card-widget=\"collapse\" title=\"Collapse\">\r\n                    <i class=\"fas fa-minus\"></i>\r\n                  </button>\r\n                  <button type=\"button\" class=\"btn btn-tool\" data-card-widget=\"remove\" title=\"Remove\">\r\n                    <i class=\"fas fa-times\"></i>\r\n                  </button>\r\n                </div>\r\n              </div>\r\n              <div class=\"card-body\">\r\n                <a href=\"/productores/nuevo\" class=\"btn btn-primary mb-3\">\r\n                    <i class=\"fas fa-plus\"></i> Nuevo registro\r\n                </a>\r\n                <a href=\"/\" type=\"button\" class=\"btn btn-secondary mb-3\"> Volver </a>\r\n                <div class=\"table-responsive\">\r\n                    <table class=\"table table-hover table-bordered\" id=\"data\">\r\n                        <thead class=\"bg-light\">\r\n                            <tr>\r\n                                <th scope=\"col\" style=\"width: 10%;\">#</th>\r\n                                <th scope=\"col\" style=\"width: 20%;\">CUIT</th>\r\n                                <th scope=\"col\" style=\"width: 20%;\">Apellidos</th>\r\n                                <th scope=\"col\" style=\"width: 20%;\">Nombres</th>\r\n                                <th scope=\"col\" style=\"width: 10%;\">Opciones</th>\r\n                            </tr>\r\n                        </thead>\r\n\r\n                        <tbody>\r\n                            ");
+		for (var productor : modelo.productores) {
+			jteOutput.writeContent("\r\n                                <tr>\r\n                                    <td scope=\"row\">");
+			jteOutput.setContext("td", null);
+			jteOutput.writeUserContent(productor.getIdProductor());
+			jteOutput.writeContent("</td>\r\n                                    <td>");
+			jteOutput.setContext("td", null);
+			jteOutput.writeUserContent(productor.getCuit());
+			jteOutput.writeContent("</td>\r\n                                    <td>");
+			jteOutput.setContext("td", null);
+			jteOutput.writeUserContent(productor.getApellidos());
+			jteOutput.writeContent("</td>\r\n                                    <td>");
+			jteOutput.setContext("td", null);
+			jteOutput.writeUserContent(productor.getNombres());
+			jteOutput.writeContent("</td>\r\n                                    <td>\r\n                                        <a type=\"button\" class=\"btn btn-warning btn-xs no-gutters\" href=\"/productores/");
+			jteOutput.setContext("a", "href");
+			jteOutput.writeUserContent(productor.getIdProductor());
+			jteOutput.writeContent("\">\r\n                                          <i class=\"fas fa-edit\"></i>\r\n                                        </a>\r\n                                        <button type=\"button\" class=\"btn btn-danger btn-xs no-gutters\" onClick=\"borrar(");
+			jteOutput.setContext("button", "onClick");
+			jteOutput.writeUserContent(productor.getIdProductor());
+			jteOutput.writeContent(")\">\r\n                                          <i class=\"fas fa-trash-alt\"></i>\r\n                                        </button>\r\n                                    </td>\r\n                                </tr>\r\n                            ");
+		}
+		jteOutput.writeContent("\r\n                        </tbody>\r\n                    </table>\r\n                </div>\r\n              </div>\r\n              ");
+		jteOutput.writeContent("\r\n            </div>\r\n            ");
+		jteOutput.writeContent("\r\n          </div>\r\n        </div>\r\n      </div>\r\n    </section>\r\n    ");
+		jteOutput.writeContent("\r\n  </div>\r\n  ");
+		jteOutput.writeContent("\r\n\r\n");
+		gg.jte.generated.ondemand.tag.JtefooterGenerated.render(jteOutput, jteHtmlInterceptor);
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		ModeloProductores modelo = (ModeloProductores)params.get("modelo");
+		render(jteOutput, jteHtmlInterceptor, modelo);
+	}
+}
--- jte-classes/gg/jte/generated/ondemand/tag/JteheadGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/tag/JteheadGenerated.java
@@ -1,15 +1,16 @@
 package gg.jte.generated.ondemand.tag;
 public final class JteheadGenerated {
 	public static final String JTE_NAME = "tag/head.jte";
-	public static final int[] JTE_LINE_INFO = {7,7,7,7,7,7,9,11,16,18,20,33};
+	public static final int[] JTE_LINE_INFO = {7,7,7,7,7,7,9,11,13,20,22,24,34};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
 		jteOutput.writeContent("<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n  <meta charset=\"utf-8\">\r\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n  <title>AdminLTE 3 | Collapsed Sidebar</title>\r\n\r\n  ");
 		jteOutput.writeContent("\r\n  <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback\">\r\n  ");
 		jteOutput.writeContent("\r\n  <link rel=\"stylesheet\" href=\"/plugins/fontawesome-free/css/all.min.css\">\r\n  ");
-		jteOutput.writeContent("\r\n  <link rel=\"stylesheet\" href=\"/plugins/adminlte/css/adminlte.min.css\">\r\n</head>\r\n\r\n<body class=\"hold-transition sidebar-mini sidebar-collapse\">\r\n");
+		jteOutput.writeContent("\r\n  <link rel=\"stylesheet\" href=\"/plugins/adminlte/css/adminlte.css\">\r\n  ");
+		jteOutput.writeContent("\r\n  <link rel=\"stylesheet\" href=\"//cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css\">\r\n  <link rel=\"stylesheet\" href=\"//cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap4.min.css\">\r\n  \r\n</head>\r\n\r\n<body class=\"hold-transition sidebar-mini sidebar-collapse\">\r\n");
 		jteOutput.writeContent("\r\n<div class=\"wrapper\">\r\n  ");
 		jteOutput.writeContent("\r\n  <nav class=\"main-header navbar navbar-expand navbar-white navbar-light\">\r\n    ");
-		jteOutput.writeContent("\r\n    <ul class=\"navbar-nav\">\r\n      <li class=\"nav-item\">\r\n        <a class=\"nav-link\" data-widget=\"pushmenu\" href=\"#\" role=\"button\"><i class=\"fas fa-bars\"></i></a>\r\n      </li>\r\n      <li class=\"nav-item d-none d-sm-inline-block\">\r\n        <a href=\"../../index3.html\" class=\"nav-link\">Home</a>\r\n      </li>\r\n      <li class=\"nav-item d-none d-sm-inline-block\">\r\n        <a href=\"#\" class=\"nav-link\">Contact</a>\r\n      </li>\r\n    </ul>\r\n  </nav>\r\n  ");
+		jteOutput.writeContent("\r\n    <ul class=\"navbar-nav\">\r\n      <li class=\"nav-item\">\r\n        <a class=\"nav-link\" data-widget=\"pushmenu\" href=\"#\" role=\"button\"><i class=\"fas fa-bars\"></i></a>\r\n      </li>\r\n      <li class=\"nav-item d-none d-sm-inline-block\">\r\n        <a href=\"/\" class=\"nav-link\">Inicio</a>\r\n      </li>\r\n    </ul>\r\n  </nav>\r\n  ");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		render(jteOutput, jteHtmlInterceptor);
