c24d7058bb3923556a562db5ad66efb9f20624d5
FacuRodriJ <facurodri00@gmail.com>
2021-11-18 03:17:18 -0300

    Ajax agregado para el pasaje de datos.

--- jte-classes/gg/jte/generated/ondemand/tag/JtefooterGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/tag/JtefooterGenerated.java
@@ -1,11 +1,10 @@
 package gg.jte.generated.ondemand.tag;
 public final class JtefooterGenerated {
 	public static final String JTE_NAME = "tag/footer.jte";
-	public static final int[] JTE_LINE_INFO = {7,7,7,7,7,7,9,11,13,15,30,35};
+	public static final int[] JTE_LINE_INFO = {7,7,7,7,7,7,9,11,13,28,33};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
 		jteOutput.writeContent("            <footer class=\"main-footer\">\r\n                <div class=\"float-right d-none d-sm-block\">\r\n                <b>Version</b> 1.0\r\n                </div>\r\n                <strong>Copyright &copy; 2021 <a href=\"/\">LaVerdeSA</a>.</strong> All rights reserved.\r\n            </footer>\r\n        </div>\r\n        ");
-		jteOutput.writeContent("\r\n\r\n        ");
-		jteOutput.writeContent("\r\n        <script src=\"/plugins/adminlte/js/jquery.min.js\"></script>\r\n        ");
+		jteOutput.writeContent("\r\n        \r\n        ");
 		jteOutput.writeContent("\r\n        <script src=\"/plugins/bootstrap/js/bootstrap.bundle.min.js\"></script>\r\n        ");
 		jteOutput.writeContent("\r\n        <script src=\"/plugins/adminlte/js/adminlte.min.js\"></script>\r\n        ");
 		jteOutput.writeContent("\r\n        <script src=\"//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js\"></script>\r\n        <script src=\"//cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js\"></script>\r\n        <script type=\"application/javascript\">\r\n            $(function () {\r\n                $('#data').DataTable({\r\n                    responsive: true,\r\n                    autoWidth: false,\r\n                    language: {\r\n                        \"url\": \"//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json\"\r\n                    }\r\n                });\r\n            });\r\n        </script>\r\n\r\n        ");
