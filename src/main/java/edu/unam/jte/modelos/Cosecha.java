534c02d92aecc37f5aa49ded0503949ca7371553
Rodrigo Lionel Antoniak <rodrigolionelantoniak@gmail.com>
2021-11-27 15:28:05 -0300

    Herencia innecesaria eliminada de Cosecha

--- src/main/java/edu/unam/jte/modelos/Cosecha.java
+++ src/main/java/edu/unam/jte/modelos/Cosecha.java
@@ -7,19 +7,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
 public class Cosecha {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int idCosecha;
 
-    private LocalDate fecha;
+    private float pesoEntrega;
+    private LocalDate tiempoEntrega;
+    private float pesoRegistro;
+    private LocalDate tiempoRegistro;
 
     @ManyToMany(targetEntity = Cuadro.class)
     private List<Cuadro> cuadros = new ArrayList<>();
@@ -30,27 +30,34 @@ public class Cosecha {
     @ManyToOne(targetEntity = Secadero.class)
     private Secadero secadero;
 
-    public Cosecha() {
-        this.setFecha(LocalDate.now());
-    }
-
-    public Cosecha(LocalDate fecha) {
-        this.setFecha(fecha);
-    }
-
-    public Cosecha(LocalDate fecha, List<Cuadro> cuadros, List<Empleado> empleados, Secadero secadero) {
-        this.setFecha(fecha);
-        this.setCuadros(cuadros);
-        this.setEmpleados(empleados);
-        this.setSecadero(secadero);
+    public Cosecha(float pesoEntrega, LocalDate tiempoEntrega, float pesoRegistro, LocalDate tiempoRegistro, List<Cuadro> cuadros, List<Empleado> empleados, Secadero secadero) {
+        this.pesoEntrega = pesoEntrega;
+        this.tiempoEntrega = tiempoEntrega;
+        this.pesoRegistro = pesoRegistro;
+        this.tiempoRegistro = tiempoRegistro;
+        this.cuadros = cuadros;
+        this.empleados = empleados;
+        this.secadero = secadero;
     }
 
     public int getIdCosecha() {
         return this.idCosecha;
     }
 
-    public LocalDate getFecha() {
-        return this.fecha;
+    public float getPesoEntrega() {
+        return this.pesoEntrega;
+    }
+
+    public LocalDate getTiempoEntrega() {
+        return this.tiempoEntrega;
+    }
+
+    public float getPesoRegistro() {
+        return this.pesoRegistro;
+    }
+
+    public LocalDate getTiempoRegistro() {
+        return this.tiempoRegistro;
     }
 
     public List<Cuadro> getCuadros() {
@@ -65,8 +72,20 @@ public class Cosecha {
         return this.secadero;
     }
 
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
+    public void setPesoEntrega(float pesoEntrega) {
+        this.pesoEntrega = pesoEntrega;
+    }
+
+    public void setTiempoEntrega(LocalDate tiempoEntrega) {
+        this.tiempoEntrega = tiempoEntrega;
+    }
+
+    public void setPesoRegistro(float pesoRegistro) {
+        this.pesoRegistro = pesoRegistro;
+    }
+
+    public void setTiempoRegistro(LocalDate tiempoRegistro) {
+        this.tiempoRegistro = tiempoRegistro;
     }
 
     public void setCuadros(List<Cuadro> cuadros) {
